b37a19e0738906452bc62f7b193311136b83249a
Ashik-Himel <ashikujjamanhimel@gmail.com>
2024-06-24 12:38:48 +0600

    baseURL changed

--- src/components/contract/ContractPaper.jsx
+++ src/components/contract/ContractPaper.jsx
@@ -22,7 +22,7 @@ export default function ContractPaper() {
     let url = window.location.href;
     url = url.replace('contract', 'contract-pdf');
 
-    axiosPublic.post('/api/contract', {url})
+    axiosPublic.post('/contract', {url})
       .then(res => {
         if (res.data?.url) {
           window.location.href = res.data.url;
