4d9cc478d91135f17f41404f00b61b06e97f96fe
Ashik-Himel <ashikujjamanhimel@gmail.com>
2024-06-27 14:53:33 +0600

    firebase keys are removed from .env

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+  apiKey: "AIzaSyCjqA6mvlcY6DsEVPiuz7ROI8Sc853Drd4",
+  authDomain: "eravend-1.firebaseapp.com",
+  projectId: "eravend-1",
+  storageBucket: "eravend-1.appspot.com",
+  messagingSenderId: "865897105182",
+  appId: "1:865897105182:web:8a58d00d8decade7066b0f",
 };
 
 const app = initializeApp(firebaseConfig);
